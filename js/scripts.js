3cb85a9ccc4505a7a1c9f699c698a28058c3b9d1
nancy nyokabi <nancynyokabi1995@gmail.com>
2021-11-22 23:59:31 +0300

    remove unwanted mail js file and format code

--- js/scripts.js
+++ js/scripts.js
@@ -21,79 +21,10 @@ $('#contacts-fm').submit(
         }
         else{myform.method=('post')
              myform.action=('https://gmail.us20.list-manage.com/subscribe/post?u=0674e772a8702cce9f1f&amp;id=f3770d27b3b7371')
-             alert('form submitted successfully')
+             alert('Name'+ 'submitted successfully')
              return true;
 
         }
     }
 )
- 
-// $(document).ready(function(){
-//     $("#work1").mouseover(function(){
-//       $("#overlay").show();
-//     }).mouseout(function(){
-//       $("#overlay").hide();
-//     });
-//   });
-//   $(document).ready(function(){
-//     $("#work2").mouseover(function(){
-//       $("#overlay2").show();
-//     }).mouseout(function(){
-//       $("#overlay2").hide();
-//     });
-//   });
-//   $(document).ready(function(){
-//     $("#work3").mouseover(function(){
-//       $("#overlay3").show();
-//     }).mouseout(function(){
-//       $("#overlay3").hide();
-//     });
-//   });
-//   $(document).ready(function(){
-//     $("#work4").mouseover(function(){
-//       $("#overlay4").show();
-//     }).mouseout(function(){
-//       $("#overlay4").hide();
-//     });
-//   });
-  
-//   $(document).ready(function(){
-//     $("#work5").mouseover(function(){
-//       $("#overlay5").show();
-//     }).mouseout(function(){
-//       $("#overlay5").hide();
-//     });
-//     $("#work6").mouseover(function(){
-//       $("#overlay6").show();
-//     }).mouseout(function(){
-//       $("#overlay6").hide();
-//     });
-//     $("#work7").mouseover(function(){
-//       $("#overlay7").show();
-//     }).mouseout(function(){
-//       $("#overlay7").hide();
-//     });
-//     $("#work8").mouseover(function(){
-//       $("#overlay8").show();
-//     }).mouseout(function(){
-//       $("#overlay8").hide();
-//     });
-//   });
-//   $(".Hover").hover(function(){
-//     $(this).animate({opacity:'1'});
-//   },
-//   function(){
-//     $(this).animate({opacity:'0'});
-//   })
-// $('.submit').click(function () {
-//         var Name = $('#mce-FNAME').val();
-//         var Email = $('#mce-EMAIL').val();
-//         var Message = $('#mce-MESSAGE');
-//         var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
-//         if (Name == '' || Email == '' || Message == '') {
-//             alert('Please make sure you have filled in the form correctly!');
-//         } else {
-//             alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
-//         }
-//   });
     
