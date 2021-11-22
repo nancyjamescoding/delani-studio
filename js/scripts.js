122f8a12b1de9880579fa2011aa39a50a9d8d226
nancy nyokabi <nancynyokabi1995@gmail.com>
2021-11-22 23:46:50 +0300

    Created a form on the contact section that includes mail chimp

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,30 @@ function toggle(div1, div2) {
     document.getElementById(div1).style.display = styleTwo
     document.getElementById(div2).style.display =  styleOne
 }
+let name=$('#name')
+        let email=$('#email')
+        let message=$('#message')
+        let myform=$('#contacts-fm')[0]
+
+$('#contacts-fm').submit(
+    function (){
+         name = name.val()
+         email = email.val()
+         message = message.val()
+
+        if(!name || !email || !message){
+            alert('fill in all the values')
+            return false;
+        }
+        else{myform.method=('post')
+             myform.action=('https://gmail.us20.list-manage.com/subscribe/post?u=0674e772a8702cce9f1f&amp;id=f3770d27b3b7371')
+             alert('form submitted successfully')
+             return true;
+
+        }
+    }
+)
+ 
 // $(document).ready(function(){
 //     $("#work1").mouseover(function(){
 //       $("#overlay").show();
